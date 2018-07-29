726eed8313a7dad29654a67bb9d56dac0c94c9aa
Mikhail Guskov <meguskov@gmail.com>
2018-07-29 15:42:30 +0300

    update README and improvement

--- src/details.js
+++ src/details.js
@@ -2,29 +2,28 @@ import { createChart } from './chart';
 
 export function getDetailsContentLayout (ymaps) {
   const BalloonContentLayout = ymaps.templateLayoutFactory.createClass(
-    `<div class="details-info">
-        {% if (properties.details) %}
-            <div class="details-info">
-                <div class="details-label">base station</div>
-                <div class="details-title">{{properties.details.serialNumber}}</div>
-                {% if (properties.details.isActive) %}
-                <div class="details-state details-state_active">active</div>
-                {% else %}
-                <div class="details-state details-state_defective">defective</div>
-                {% endif %}
-                <div class="details-state details-state_connections">
-                    connections: {{properties.details.connections}}
-                </div>
+    `{% if (properties.details) %}
+        <div class="details-info">
+            <div class="details-label">base station</div>
+            <div class="details-title">{{properties.details.serialNumber}}</div>
+            {% if (properties.details.isActive) %}
+            <div class="details-state details-state_active">active</div>
+            {% else %}
+            <div class="details-state details-state_defective">defective</div>
+            {% endif %}
+            <div class="details-state details-state_connections">
+                connections: {{properties.details.connections}}
             </div>
-            <div class="details-info">
-                <div class="details-label">connections</div>
-                <canvas class="details-chart" width="270" height="100" />
-            </div>
-        {% else %}
-            <div class="details-info">
-                Идет загрузка данных...
-            </div>
-        {% endif %}
+        </div>
+        <div class="details-info">
+            <div class="details-label">connections</div>
+            <canvas class="details-chart" width="270" height="100" />
+        </div>
+      {% else %}
+        <div class="details-info">
+            Идет загрузка данных...
+        </div>
+      {% endif %}
     `,
     {
       build () {
